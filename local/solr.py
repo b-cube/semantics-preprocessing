daf2c357da5bb8faad57c4ab990d6f76f36d8248
Soren Scott <soren.scott@nsidc.org>
2015-01-30 15:03:30 -0700

    initial commit
    text processing from the nutch harvests

---
+++ local/solr.py
@@ -0,0 +1,17 @@
+import os
+import json
+import requests
+
+'''
+pull a record/recordset from the initial solr results
+to parse for the triples or for the intermediate clean text inputs
+'''
+
+_url = 'http://xx.xxx.xxxx.xxx:xxxxx/solr/#/collection1/query'
+
+_u = 'xxxx'
+_p = 'xxxx'
+
+
+
+
