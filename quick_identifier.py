5a9aa8076afb57e6b31d9ec05135461cb57803d0
Soren Scott <soren.scott@nsidc.org>
2015-02-04 13:18:53 -0700

    all of the easily identifiable things.

--- quick_identifier.py
+++ quick_identifier.py
@@ -35,10 +35,16 @@ def identify(raw_content):
 		return 'ATOM'
 	elif 'http://www.opengis.net/wms' in raw_content:
 		return 'WMS'
+	elif 'http://www.opengis.net/wmts/1.0' in raw_content:
+		return 'WMTS'
 	elif 'http://www.opengis.net/wfs' in raw_content:
 		return 'WFS'
 	elif 'http://www.opengis.net/wcs' in raw_content:
 		return 'WCS'
+	elif 'http://www.opengis.net/swe/1.0.1' in raw_content:
+		return 'SWE'
+	elif 'http://www.opengis.net/' in raw_content:
+		return 'Unidentified OGC'
 	elif 'http://gcmd.gsfc.nasa.gov/Aboutus/xml/dif/' in raw_content:
 		return 'DIF'
 	elif 'http://www.openarchives.org/OAI/' in raw_content:
@@ -53,7 +59,7 @@ def identify(raw_content):
 		return 'FGDC-1998'
 	elif '<metstdv>FGDC-STD-012-2002' in raw_content:
 		return 'FGDC-2002'
-	elif 'http://www.incident.com/cap/1.0' in raw_content:
+	elif 'http://www.incident.com/cap/1.0' in raw_content or 'urn:oasis:names:tc:emergency:cap:' in raw_content:
 		'''
 		usgs alerts
 			view-source:http://www.usgs.gov/alerts/cap/USGS-landslides.20060831T184846
@@ -68,6 +74,30 @@ def identify(raw_content):
 		return 'MS Office'
 	elif 'http://www.unidata.ucar.edu/namespaces/netcdf/ncml-2.2' in raw_content:
 		return 'NetCDF'
+	elif '<rss version="' in raw_content or 'http://api.npr.org/nprml' in raw_content or 'rss' in data['id']:
+		return 'RSS'	
+	elif 'http://archipelago.phrasewise.com/rsd' in raw_content or '?rsd' in data['id'] or 'rsd.xml' in data['id']:
+		return 'WordPress'	
+	elif 'http://www.loc.gov/METS_Profile/' in raw_content:
+		return 'LOC-METS'
+	elif 'http://datacite.org/schema/' in raw_content:
+		return 'DataCite'
+	elif 'eml://ecoinformatics.org/eml-' in raw_content:
+		return 'EML'
+	elif 'http://anss.org/xmlns/catalog/0.1' in raw_content or 'http://anss.org/xmlns/tensor/0.1' in raw_content or 'http://quakeml.org/xmlns/bed/1.2' in raw_content:
+		return 'USGS Quake'
+	elif 'http://niem.gov/' in raw_content:
+		return 'Niem (Foia)'
+	elif 'http://schemas.xmlsoap.org/disco/' in raw_content:
+		return 'Disco'
+	elif 'http://schemas.xmlsoap.org/soap/envelope/' in raw_content:
+		return 'Soap'
+	elif 'http://www.google.com/geo/schemas/sitemap/1.0' in raw_content or 'http://www.sitemaps.org/schemas/sitemap/' in raw_content:
+		return 'Sitemap'
+	elif 'http://www.itunes.com/dtds/podcast-1.0.dtd' in raw_content:
+		return 'iTunes'
+	elif 'http://www.cuahsi.org/waterML/' in raw_content:
+		return 'WaterML'
 
 	return ''
 
