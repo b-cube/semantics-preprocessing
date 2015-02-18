14e2c380c32642544d3b77b5c00b882cf56d5b0d
Soren Scott <soren.scott@nsidc.org>
2015-02-18 10:39:18 -0700

    generic filter handling

--- lib/identifier.py
+++ lib/identifier.py
@@ -1,4 +1,6 @@
 import logging
+import yaml
+import re
 
 LOGGER = logging.getLogger(__name__)
 
@@ -21,104 +23,58 @@ LOGGER = logging.getLogger(__name__)
 # add the bit about it's valid xml but a error response
 
 
-def identify_response(source_content, source_url):
-    '''
-    from a url and some response content, try:
-        to identify what kind of service, version
-        of the service (this is very tricky without
-            parsing the xml - namespaces and urls
-            cannot be trusted), type of response (catalog,
-            dataset, visualization, whatever)
-    '''
+def identify(yaml_file, source_content, source_url):
+    def _parse_yaml(self):
+        with open(yaml_file, 'r') as f:
+            text = f.read()
+        return yaml.load(text)
 
-    def _in_url(url, filters):
-        '''
-        check for some string in a url
-        (not concerned so much about where)
-        '''
-        url = url.strip()
-        return len([f for f in filters if f.strip() in url]) > 0
+    def _filter(operator, filters):
+        clauses = []
+        for f in filters:
+            filter_object = source_content if f['object'] == 'content' else source_url
+            filter_value = f['value']
+            filter_type = f['type']
 
-    def _in_content(content, filters):
-        '''
-        check for some string in a text blob
-        (not concerned so much about where)
-        '''
-        return len([f for f in filters if f in content]) > 0
+            if filter_type == 'simple':
+                clauses.append(filter_value in filter_object)
+            elif filter_type == 'regex':
+                clauses.append(len(re.findall(filter_value, filter_object)) > 0)
+            elif filter_type == 'complex':
+                filter_operator = f['operator']
+                clauses.append(_filter(filter_operator, f['filters']))
 
-    def identify_protocol():
-        '''
-        basic identification
+        if operator == 'ands':
+            # everything must be true
+            return sum(clauses) == len(clauses)
+        elif operator == 'ors':
+            # any one must be true
+            return sum(clauses) > 0
 
-        note:
-            currently starting with high priority service types
-        '''
-        if _in_content(source_content, ['http://www.isotc211.org/2005/gmi',
-                                        'http://www.isotc211.org/2005/gmd']):
-            # note: not differentiating between versions here
-            return 'ISO-19115'
-        elif _in_content(source_content, ['http://www.unidata.ucar.edu/namespaces/thredds/InvCatalog/v1.0']):
-            return 'UNIDATA:Catalog'
-        elif _in_content(source_content, ['http://a9.com/-/spec/opensearch/1.1/']):
-            return 'OpenSearch1.1'
-        elif _in_content(source_content, ['http://wadl.dev.java.net']):
-            return 'WWW:WADL'
-        elif _in_content(source_content, ['http://www.opengis.net/wms']) \
-                or _in_url(source_url.upper(), ['SERVICE=WMS']) \
-                or (_in_content(source_content, ['http://mapserver.gis.umn.edu/mapserver'])
-                    and _in_url(source_url.upper(), ['SERVICE=WMS'])):
-            return 'OGC:WMS'
-        elif _in_content(source_content, ['http://www.opengis.net/wfs']) \
-                or _in_url(source_url.upper(), ['SERVICE=WFS']) \
-                or (_in_content(source_content, ['http://mapserver.gis.umn.edu/mapserver'])
-                    and _in_url(source_url.upper(), ['SERVICE=WFS'])):
-            return 'OGC:WFS'
-        elif _in_content(source_content, ['http://www.opengis.net/wcs']) \
-                or _in_url(source_url.upper(), ['SERVICE=WCS']) \
-                or (_in_content(source_content, ['http://mapserver.gis.umn.edu/mapserver'])
-                    and _in_url(source_url.upper(), ['SERVICE=WCS'])):
-            return 'OGC:WCS'
-        elif _in_content(source_content, ['http://www.openarchives.org/OAI/']):
-            return 'OAI-PMH'
+    def _identify_protocol():
+        for protocol in protocols:
+            protocol_filters = protocol['filters']
 
-        LOGGER.info('Unable to identify: %s' % source_url)
+            for k, v in protocol_filters.iteritems():
+                is_match = _filter(k, v)
+                if is_match:
+                    return protocol['name']
 
         return ''
 
-    def identify_as_service(protocol):
-        '''
-        based on the protocol and source information, identify whether
-        a response/service is an actual service description document or
-        some other thing
-        '''
-        if protocol in ['OGC:WMS', 'OGC:WFS', 'OGC:WCS'] and \
-                _in_url(source_url.upper(), ['REQUEST=GETCAPABILITIES']):
-            return True
-        elif protocol in ['OAI-PMH'] and (_in_url(source_url.upper(),
-                ['VERB=IDENTIFY']) or _in_content(source_content, ['<Identify>'])):
-            return True
-        elif protocol in ['OpenSearch'] and _in_content(source_content,
-                ['OpenSearchDescription']):
-            return True
-        elif protocol in ['THREDDS Catalog']:
-            return True
+    def _identify_service_of_protocol(protocol):
+        protocol_data = next(p for p in protocols if p['name'] == protocol)
+        if not protocol_data:
+            LOGGER.warn('failed to identify protocol %s' % protocol)
+            return False
 
-        return False
+        for service in protocol_data['services']:
+            for k, v in service['filters'].iteritems():
+                is_match = _filter(k, v)
+                if is_match:
+                    return service['name']
 
-    def identify_as_dataset(protocol):
         return ''
 
-    def generate_urn(protocol, service, dataset, version):
-        return ':'.join([protocol, service, dataset, version])
-
-    # identify protocol
-    # identify service type
-    # ignore the version?
-    # identify response type
-
-    protocol = identify_protocol(source_content, source_url)
-    service = identify_as_service(protocol, source_content, source_url)
-    dataset = identify_as_dataset(protocol, source_content, source_url)
-    version = ''
-
-    return generate_urn(protocol, service, dataset, version)
+    config_data = _parse_yaml()
+    protocols = config_data['protocols']
