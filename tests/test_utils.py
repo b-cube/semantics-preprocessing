2be078d1a595bfffe98343c28cae08fd9761cb4d
Soren Scott <soren.scott@nsidc.org>
2015-03-03 22:05:57 -0700

    a very basic keyword string normalization
    replace some known delimiters with a comma for generic handling re:
    arrays in triples (really, multiple elements in triples) and for nap
    input (tokenizers).

---
+++ tests/test_utils.py
@@ -0,0 +1,30 @@
+import unittest
+from lib.utils import normalize_keyword_text
+
+
+class TestKeywords(unittest.TestCase):
+    def setUp(self):
+        '''
+        nothing to set up
+        '''
+        pass
+
+    def test_strings(self):
+        '''
+        the most basic examples
+        '''
+        strings = [
+            'soil+clay+pedon+loam+sandy loam',
+            'soil-clay-pedon-loam-sandy loam',
+            'soil;clay;pedon;loam;sandy loam',
+            'soil|clay|pedon|loam|sandy loam',
+            'soil>clay>pedon>loam>sandy loam'
+        ]
+
+        tests = []
+        for s in strings:
+            tests.append(normalize_keyword_text(s))
+
+        self.assertTrue(tests is not None)
+        self.assertTrue(len(set(tests)) == 1)
+        self.assertTrue('+' not in next(iter(set(tests))))
