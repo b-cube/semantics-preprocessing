71a33e0b8cf379a4c38ba3b9966130117203401a
Soren Scott <soren.scott@nsidc.org>
2015-03-05 16:03:22 -0700

    add mods for iso, etc, to complete listing

--- identify_priority_services.py
+++ identify_priority_services.py
@@ -13,7 +13,7 @@ process against a local file store of solr responses
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-YAML_FILE = 'lib/configs/identifiers.yaml'
+YAML_FILE = 'lib/configs/all_identifiers.yaml'
 
 # responses = glob.glob('testdata/docs/response_60de9ec6341a2116ff4bb2739c307739.json')
 responses = glob.glob('testdata/docs/response_*.json')
@@ -22,8 +22,8 @@ responses = glob.glob('testdata/docs/response_*.json')
 #     digests = f.readlines()
 # responses = ['testdata/docs/response_%s.json' % d.strip() for d in digests]
 
-with open('priority_identification.csv', 'w') as f:
-    f.write('digest|url|protocol|service|has dataset|has metadata|version|is error\n')
+with open('priority_identification_all.csv', 'w') as f:
+    f.write('digest|url|protocol|subtype|service|has dataset|has metadata|version|is error\n')
 
 for response in responses:
     with open(response, 'r') as f:
@@ -48,6 +48,7 @@ for response in responses:
     identifier = Identify(YAML_FILE, cleaned_text, url, **{'parser': parser, 'ignore_case': True})
     identifier.identify()
     protocol = identifier.protocol
+    subtype = identifier.subtype
     service = identifier.service
     has_dataset = identifier.has_dataset
     has_metadata = identifier.has_metadata
@@ -57,6 +58,6 @@ for response in responses:
     # if not protocol:
     #     continue
 
-    with open('priority_identification.csv', 'a') as f:
-        f.write('|'.join([digest, url.replace(',', ';').replace('|', ';'), protocol, service,
+    with open('priority_identification_all.csv', 'a') as f:
+        f.write('|'.join([digest, url.replace(',', ';').replace('|', ';'), protocol, subtype, service,
                 str(has_dataset), str(has_metadata), str(version), str(is_error)]) + '\n')
