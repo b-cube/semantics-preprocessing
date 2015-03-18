ca5493ab6eed4f976b91da1ad6781ac240fa7c62
Soren Scott <soren.scott@nsidc.org>
2015-03-18 00:09:09 -0600

    not so broken thredds but still not quite right
    also needs a better way of handling children. except non-standard
    standard, etc.

--- lib/preprocessors/thredds_preprocessors.py
+++ lib/preprocessors/thredds_preprocessors.py
@@ -1,4 +1,5 @@
 from lib.base_preprocessors import BaseReader
+from itertools import chain
 
 
 class ThreddsReader(BaseReader):
@@ -77,22 +78,52 @@ class ThreddsReader(BaseReader):
             return dataset_description
 
         def _get_metadata_elements(elem):
+            '''
+            <metadata xlink:href="http://data.eol.ucar.edu/jedi/catalog/ucar.ncar.eol.project.
+                ARCSS.metadata.xml" metadataType="THREDDS" inherited="false"/>
+
+            there is no identifier for the metadata (for the inherited kind)
+            '''
             # for any metadata element, and they are differently structured
             # could be nested at the catalogRef or dataset element level
-            pass
+            metadata_description = elem.attrib
+
+            # and then go through for any child element
+
+            return metadata_description
+
+        def _get_catalog_ref(elem):
+            '''
+            <thredds:catalogRef name="2000" xlink:href="2000/catalog.xml" xlink:title="2000"
+                xlink:type="simple" ID="/opendap/hyrax/allData/55/MOD17A3/2000/"/>
+            '''
+            return elem.attrib
 
         services = self.parser.find(svc_xpath)
+        # ffs, services can be nested too
         if services:
             self._to_exclude.append(svc_xpath[1:])
-            self._to_exclude.append(svc_xpath[1:] + '/@name')
-            self._to_exclude.append(svc_xpath[1:] + '/@serviceType')
-            self._to_exclude.append(svc_xpath[1:] + '/@base')
+            for key in services[0].attrib.keys():
+                self._to_exclude.append(svc_xpath[1:] + '/@' + key)
+
             for service in services:
-                endpoints.append({
-                    "name": service.attrib.get('name', ''),
-                    "type": service.attrib.get('serviceType', ''),
-                    "relative_path": service.attrib.get('base', '')
-                })
+                service_endpoint = service.attrib
+                children = service.xpath('*[local-name()="service"]')
+                endpoints += [
+                    dict(
+                        chain(
+                            child.attrib.items,
+                            {
+                                "childOf": service_endpoint['ID']
+                            }.items()
+                        )
+                    ) for child in children
+                ]
+                if children:
+                    service_endpoint['parentOf'] = []
+                    service_endpoint['parentOf'] += [
+                        child.attrib['ID'] for child in children
+                    ]
 
         # get the level-one children (catalog->child)
         datasets = self.parser.find(dataset_xpath)
@@ -115,44 +146,68 @@ class ThreddsReader(BaseReader):
                 # and dataset description values (it's a singleton)
                 d = _get_dataset_elements(dataset)
 
-                endpoint = {
+                dataset_endpoint = {
                     "name": d['name'],
                     "id": d['id']
                 }
 
                 if 'datasize' in d.keys():
-                    endpoint["dataset_size"] = d['datasize']['size'],
-                    endpoint["dataset_size_units"] = d['datasize']['units']
+                    dataset_endpoint["dataset_size"] = d['datasize']['size'],
+                    dataset_endpoint["dataset_size_units"] = d['datasize']['units']
 
                 if 'date' in d.keys():
-                    endpoint["date"] = d['date']['date']
-                    endpoint["date_type"] = d['date']['type']
+                    dataset_endpoint["date"] = d['date']['date']
+                    dataset_endpoint["date_type"] = d['date']['type']
 
                 if 'access' in d.keys():
-                    endpoint["relative_path"] = d['access']['url']
-                    endpoint["relative_path"] = d['access']['serviceName']
-                    endpoints.append(endpoint)
+                    dataset_endpoint["relative_path"] = d['access']['url']
+                    dataset_endpoint["relative_path_name"] = d['access']['serviceName']
 
-                # update the excludes
-                # self._to_exclude += [
-                #     dataset_xpath[1:] + '/*[local-name()="access"]/@serviceName',
-                #     dataset_xpath[1:] + '/*[local-name()="access"]/@urlPath',
-                #     dataset_xpath[1:] + '/*[local-name()="date"]/@type',
-                #     dataset_xpath[1:] + '/*[local-name()="date"]',
-                #     dataset_xpath[1:] + '/*[local-name()="dataSize"]/@units',
-                #     dataset_xpath[1:] + '/*[local-name()="dataSize"]'
-                # ]
+                dataset_children = []
 
                 # does it contain a metadata child?
                 metadata_elem = next(iter(dataset.xpath('*[local-name()="metadata"]')), None)
-                if metadata_elem is mot None:
+                if metadata_elem is not None:
                     # get the link and type for a new endpoint.
-
+                    metadata = _get_metadata_elements(metadata_elem)
+                    if 'href' in metadata:
+                        metadata.update({"childOf": d['id']})
+                        endpoints += [metadata]
+                    else:
+                        # there's no identifiable endpoint
+                        dataset_endpoint['metadata'] = metadata
 
                 # does it contain catalogRef children?
+                catalog_refs = dataset.xpath('*[local-name()="catalogRef"]')
+                endpoints += [
+                    dict(
+                        chain(
+                            _get_catalog_ref(cr).items(),
+                            {
+                                "childOf": d['id']
+                            }.items()
+                        )
+                    ) for cr in catalog_refs
+                ]
+
+                dataset_children += [cr.attrib['ID'] for cr in catalog_refs]
 
                 # does it contain dataset children?
+                children = dataset.xpath('*[local-name()="dataset"]')
+                endpoints += [
+                    dict(
+                        chain(
+                            _get_dataset_elements(c).items(),
+                            {
+                                "childOf": d['id']
+                            }.items()
+                        )
+                    ) for c in children
+                ]
+                dataset_children += [c.attrib['ID'] for c in children]
 
+                if dataset_children:
+                    d['parentOf'] = dataset_children
 
         catrefs = self.parser.find(catref_xpath)
         if catrefs:
