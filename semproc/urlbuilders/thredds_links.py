e9d9d89a6768451859523b6a3f5cb04ef06e728d
Soren Scott <soren.scott@nsidc.org>
2015-06-25 00:21:05 -0600

    shifting the thredds link builder to a singleton class
    it is not wholly wired.

--- semproc/urlbuilders/thredds_links.py
+++ semproc/urlbuilders/thredds_links.py
@@ -13,9 +13,6 @@ class ThreddsLinkBuilder(object):
     def __init__(self, source_url, source_content):
         self.source_url = source_url
         self.source_content = source_content
-
-        self.source_parts = urlparse.urlparse(source_url)
-
         self._parse()
 
     def _parse(self):
@@ -29,6 +26,66 @@ class ThreddsLinkBuilder(object):
             self.xml = etree.fromstring(self.source_content, parser=parser)
         except:
             self.xml = None
+
+    def generate_urls(self):
+        '''
+        run the xml
+        '''
+        if self.xml is None:
+            return []
+
+        services = self.xml.xpath(
+            '//*[local-name()="service" and @base != ""]/@*[local-name()="base"]')
+        services = [s[:-1] if s.endswith('/') else s for s in services]
+
+        elements = self.xml.xpath('//*[local-name()="catalogRef" or local-name()="dataset"]')
+
+        # return the urls as id: new url(s) (based on the services)
+        generated_urls = {}
+        for element in elements:
+            # track the ID or the name is that's not provided
+            elem_id = element.attrib.get('ID', '')
+            if not elem_id:
+                elem_id = element.attrib.get('name', '')
+
+            if not elem_id:
+                continue
+            tl = ThreddsLink(element, self.source_url, services)
+
+            generated_urls[elem_id] = tl.urls
+
+        return generated_urls
+
+
+class ThreddsLink():
+    def __init__(self, elem, source_url, services=[]):
+        self.elem = elem
+        self.services = services
+        self.source_url = source_url
+        self.source_parts = urlparse.urlparse(source_url)
+
+    def _generate(self):
+        tag = extract_element_tag(self.elem.tag)
+
+        # find the href or urlPath
+        if tag == 'dataset':
+            href = self.elem.attrib.get('urlPath', None)
+            if href is None:
+                href = next(
+                    iter(self.elem.xpath('*[local-name()="access"]/@*[local-name()="urlPath"]')),
+                    None)
+
+            # if there's a urlPath (anywhere), it *should* be the
+            # terminal file path
+            hrefs = [
+                self._generate_url('/'.join(service, href), self._get_ogc_params(service))
+                for service in self.services]
+
+        elif tag == 'catalogRef':
+            href = self.elem.attrib.get('{http://www.w3.org/1999/xlink}href', '')
+            hrefs = [self._generate_url(href)]
+
+        self.urls = hrefs
 
     def _generate_url(self, rel_path, query_params={}):
         if urlparse.urlparse(rel_path).scheme:
@@ -66,51 +123,3 @@ class ThreddsLinkBuilder(object):
             return {'service': 'wcs', 'request': 'getcapabilities', 'version': '1.1.2'}
         else:
             return None
-
-    def generate_urls(self):
-        '''
-        run the xml
-        '''
-        if self.xml is None:
-            return []
-
-        services = self.xml.xpath(
-            '//*[local-name()="service" and @base != ""]/@*[local-name()="base"]')
-        services = [s[:-1] if s.endswith('/') else s for s in services]
-
-        elements = self.xml.xpath('//*[local-name()="catalogRef" or local-name()="dataset"]')
-
-        # return the urls as id: new url(s) (based on the services)
-        generated_urls = {}
-        for element in elements:
-            tag = extract_element_tag(element.tag)
-
-            # track the ID or the name is that's not provided
-            elem_id = element.attrib.get('ID', '')
-            if not elem_id:
-                elem_id = element.attrib.get('name', '')
-
-            if not elem_id:
-                continue
-
-            # find the href or urlPath
-            if tag == 'dataset':
-                href = element.attrib.get('urlPath', None)
-                if href is None:
-                    href = next(
-                        iter(element.xpath('*[local-name()="access"]/@*[local-name()="urlPath"]')),
-                        None)
-
-                # if there's a urlPath (anywhere), it *should* be the
-                # terminal file path
-                hrefs = [
-                    self._generate_url('/'.join(service, href), self._get_ogc_params(service))
-                    for service in services]
-
-            elif tag == 'catalogRef':
-                href = element.attrib.get('{http://www.w3.org/1999/xlink}href', '')
-                hrefs = [self._generate_url(href)]
-
-            generated_urls[elem_id] = hrefs
-
-        return generated_urls
